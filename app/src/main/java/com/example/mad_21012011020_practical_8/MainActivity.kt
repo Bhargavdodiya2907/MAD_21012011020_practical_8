c1f601355c40b14196834423c9c526ccc518737b
student <student@gnu.ac.in>
2023-09-25 08:48:10 +0530

    pr8

--- app/src/main/java/com/example/mad_21012011020_practical_8/MainActivity.kt
+++ app/src/main/java/com/example/mad_21012011020_practical_8/MainActivity.kt
@@ -1,14 +1,14 @@
 package com.example.mad_21012011020_practical_8
 
+import android.app.AlarmManager
 import android.app.PendingIntent
 import android.app.TimePickerDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.card.MaterialCardView
+import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,15 +19,35 @@ class MainActivity : AppCompatActivity() {
         val alarmbutton=findViewById<MaterialButton>(R.id.clock)
         alarmbutton.setOnClickListener {
             //card.visibility=View.VISIBLE
-            TimePickerDialog(this,{tp,hour,minute->setalarmtime(hour, minute)},11,0,false).show()
+            TimePickerDialog(this,{tp,hour,minute->setalarmtime(hour, minute)},Calendar.HOUR,Calendar.MINUTE,false).show()
         }
 
     }
-    fun setalarmtime(hour:Int,minute:Int){
+    fun stop(){
+        setalarm(-1,AlarmBroadcastReceiver.Alarmstop)
+    }
+    fun setalarmtime(hour:Int, minute: Int){
         //card.visibility=View.GONE
+        val alarmtime=Calendar.getInstance()
+        val year=alarmtime.get(Calendar.YEAR)
+        val month=alarmtime.get(Calendar.MONTH)
+        val date=alarmtime.get(Calendar.DATE)
+        val minute=alarmtime.get(Calendar.MINUTE)
+        val hour=alarmtime.get(Calendar.HOUR)
+        alarmtime.set(year,month, date,minute)
+        setalarmtime(alarmtime.timeInMillis,AlarmBroadcastReceiver.Alarmstart)
     }
     fun setalarm(militime:Long,action:String){
         val intentalarm=Intent(applicationContext,AlarmBroadcastReceiver::class.java)
+        intentalarm.putExtra(AlarmBroadcastReceiver.Alarmkey,action)
         val pendingIntent=PendingIntent.getBroadcast(applicationContext,4356,intentalarm,PendingIntent.FLAG_UPDATE_CURRENT)
+        val alarmmanager=getSystemService(ALARM_SERVICE) as AlarmManager
+        if (action==AlarmBroadcastReceiver.Alarmstart){
+            alarmmanager.setExact(AlarmManager.RTC_WAKEUP,militime,pendingIntent)
+        }
+        else if (action==AlarmBroadcastReceiver.Alarmstop){
+            alarmmanager.cancel(pendingIntent)
+            sendBroadcast(intentalarm)
+        }
     }
 }
